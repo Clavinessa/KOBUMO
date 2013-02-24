0d8faa0a7930858f2ab53842b89e908120079d4c
Diana Brusnikina <Clavinessa@gmail.com>
2013-02-24 19:24:07 +0600

    Pure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KOBUMO::Application.config.secret_token = '1e4a7ad0d8528b3bbf9b6c4ddf614392ea315ecb730cbc39b223eaae3089a8ecce8dbed7e4faf815235570292bf1f86ea671abcdef37f212df8b61c9911582d2'
